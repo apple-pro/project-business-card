ab8c0267fbcaf9e70e271d28886cf9ad75701376
LM Bibera <lyndon.bibera@headhuntr.io>
2020-08-31 21:14:49 +0800

    add image

--- BusinessCard/ContentView.swift
+++ BusinessCard/ContentView.swift
@@ -11,13 +11,19 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Color(.red)
+            Color(red: 0.15, green: 0.24, blue: 0.46)
                 .edgesIgnoringSafeArea(.all)
             
             VStack {
                 
+                Image("john wick")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150)
+                    .clipShape(Circle())
+                
                 Text("John Wick")
-                    .font(Font.custom("JohnWick", size: 40))
+                    .font(Font.custom("JohnWick", size: 70))
                     .bold()
                     .foregroundColor(.white)
                 
